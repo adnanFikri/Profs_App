02704cf945f12739fbafed6bd3aec7345ad1881a
adnanFikri02 <adnan.fikrri@gmail.com>
2024-01-01 23:22:28 +0100

    add posibility of first auth user will be admin

--- Prof_App/app/Http/Controllers/Auth/RegisterController.php
+++ Prof_App/app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        if(User::count() === 1){
+            $user->Admin = true;
+            $user->save();
+        };
+
+        return $user;
     }
 }
